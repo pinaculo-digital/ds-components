b022be4a40f7b318ca92d0b82bb7bb4fd9fad7a4
Kaio Moura <kaio@pinaculodigital.com.br>
2025-11-18 12:40:12 -0300

    feat: inclusão de componentes de navegação

---
+++ src/components/navigation/components/Header.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import type { Component, Snippet } from 'svelte';
+
+  interface Props {
+    label: string;
+    desc: string;
+    lateralPadding?: boolean;
+    snippetRight?: Component | Snippet;
+  }
+
+  let { label, desc, snippetRight: Snip, lateralPadding = false }: Props = $props();
+</script>
+
+<div class="border-b border-soft-200 pb-5 flex items-center justify-between bg-white-0 {lateralPadding ? 'px-5' : ''}">
+  <div class="flex flex-col">
+    <span class="text-[18px] font-medium">
+      {label}
+    </span>
+    <p class="text-14 text-sub-600">
+      {desc}
+    </p>
+  </div>
+  {#if Snip}
+    <Snip />
+  {/if}
+</div>
---
+++ src/components/navigation/components/HeaderPage.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import type { Snippet } from 'svelte';
+
+  interface Props {
+    label: string;
+    description: string;
+    children?: Snippet;
+  }
+
+  let { label, description, children }: Props = $props();
+</script>
+
+<div
+  class="border-soft-200 sticky top-0 z-99 flex w-full justify-between gap-4 border-b bg-white shadow-[0_4px_6px_1px_rgba(255,255,255,1)]"
+>
+  <div class="flex flex-col gap-1 py-5">
+    <p class="text-label-large text-strong-950">{label}</p>
+    <p class="text-label-small text-sub-600">{description}</p>
+  </div>
+  {#if children}
+    <div class="flex h-full w-fit py-5">
+      {@render children()}
+    </div>
+  {/if}
+</div>
